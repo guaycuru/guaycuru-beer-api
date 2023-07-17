cb64b19ac89f673751b2453edfbbd35b5c42b06a
Felipe Guaycuru <guaycuru@gmail.com>
2023-07-17 12:07:46 -0300

    Atualiza update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git pull
-bin/doctrine orm:schema-tool:update --dump-sql --force
 bin/doctrine orm:clear-cache:metadata
-bin/doctrine orm:generate-proxies
 bin/doctrine orm:clear-cache:query
+bin/doctrine orm:schema-tool:update --dump-sql --force
+bin/doctrine orm:generate-proxies
